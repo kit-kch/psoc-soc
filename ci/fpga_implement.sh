e55b31bd978eb56b099333a8be8aee4d9ddd5348
Julius Ring <julius.ring@student.kit.edu>
2018-12-17 04:40:08 +0100

    Let CI scripts copy some interesting logs into the artifacts folder

--- ci/fpga_implement.sh
+++ ci/fpga_implement.sh
@@ -10,11 +10,19 @@ case "$COMMAND" in
           echo "Testing standalone implementation"
           vivado -mode batch -source "ci/fpga_standalone.tcl" -nojournal -log "fpga_standalone.log"
           cp -v psoc_fpga/psoc_fpga.runs/impl_1/fpga_standalone_top.bit out/
+	  cp -v psoc_fpga/psoc_fpga.runs/impl_1/fpga_standalone_top_timing_summary_routed.rpt out/
+	  cp -v psoc_fpga/psoc_fpga.runs/impl_1/fpga_standalone_top_utilization_placed.rpt out/
+	  cp -v psoc_fpga/psoc_fpga.runs/synth_1/runme.log out/synthesis.log
+	  cp -v psoc_fpga/psoc_fpga.runs/impl_1/runme.log out/implementation.log
           ;;
       implement_soc)
           echo "Testing SOC implementation"
           vivado -mode batch -source "ci/fpga_soc.tcl" -nojournal -log "fpga_soc.log"
           cp -v psoc_fpga/psoc_fpga.runs/impl_1/fpga_riscv_top.bit out/
+	  cp -v psoc_fpga/psoc_fpga.runs/impl_1/fpga_riscv_top_timing_summary_routed.rpt out/
+	  cp -v psoc_fpga/psoc_fpga.runs/impl_1/fpga_riscv_top_utilization_placed.rpt out/
+	  cp -v psoc_fpga/psoc_fpga.runs/synth_1/runme.log out/synthesis.log
+	  cp -v psoc_fpga/psoc_fpga.runs/impl_1/runme.log out/implementation.log
           ;;
       *)
           printf "Unknown command: '%s'\n\n" "$COMMAND" >&2
