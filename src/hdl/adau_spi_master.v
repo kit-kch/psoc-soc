7359e4483f99fc05a135b4ba4179ec8651387580
Johannes Pfau <johannes.pfau@kit.edu>
2019-10-23 18:23:14 +0200

    Rework SPI testbench

--- src/hdl/adau_spi_master.v
+++ src/hdl/adau_spi_master.v
@@ -1,81 +1,84 @@
-`timescale 1ns/1ps
+    `timescale 1ns/1ps
 
-module adau_spi_master
-  (input clk,
-   input reset,
+    module adau_spi_master
+    (input clk,
+    input reset,
 
-   input [31:0] data_in,
-   input valid,
-   output ready,
+    input [31:0] data_in,
+    input valid,
+    output ready,
 
-   output cdata,
-   output cclk,
-   output reg clatch_n);
+    output cdata,
+    output cclk,
+    output reg clatch_n);
 
-   // CLK runs at 120MHz. We use a counter to generate a pulse every 6
-   // CLKs. If we toggle CCLK on each pulse we get a 10MHz signal -
-   // the maximum CCLK frequency.
-   localparam CCLK_DIVIDER = 6;
-   localparam DIVIDER_BITS = $clog2(CCLK_DIVIDER);
-   reg [CCLK_DIVIDER-1:0] divider;
-   wire divider_tick = divider == (CCLK_DIVIDER - 1);
+    // CLK runs at 120MHz. We use a counter to generate a pulse every 6
+    // CLKs. If we toggle CCLK on each pulse we get a 10MHz signal -
+    // the maximum CCLK frequency.
+    localparam CCLK_DIVIDER = 6;
+    localparam DIVIDER_BITS = $clog2(CCLK_DIVIDER);
+    reg [CCLK_DIVIDER-1:0] divider;
+    wire divider_tick = divider == (CCLK_DIVIDER - 1);
 
-   // 2 states per bit (clock high, clock low) times 32 bits per
-   // transfer gives 64 states. We need another wait state after
-   // pulling CLATCH_N high, so we respect the minimum pulse width on that
-   // signal. Finally, we need an idle state, where READY is asserted.
-   // Therefore, during a transfer, our FSM steps through these states
-   // in order:
-   localparam BIT_31_CLOCK_LOW  = 0;
-   localparam BIT_31_CLOCK_HIGH = 1;
-   localparam BIT_30_CLOCK_LOW  = 2;
-   // ...
-   localparam BIT_0_CLOCK_HIGH = 62;
-   localparam CLATCH_N_GOING_HIGH = 63;  // AKA BIT_0_CLOCK_LOW
-   localparam WAITING_FOR_CLATCH_N = 64;
-   localparam IDLE = 65;
+    // 2 states per bit (clock high, clock low) times 32 bits per
+    // transfer gives 64 states. We need another wait state after
+    // pulling CLATCH_N high, so we respect the minimum pulse width on that
+    // signal. Finally, we need an idle state, where READY is asserted.
+    // Therefore, during a transfer, our FSM steps through these states
+    // in order:
+    localparam BIT_31_CLOCK_LOW  = 0;
+    localparam BIT_31_CLOCK_HIGH = 1;
+    localparam BIT_30_CLOCK_LOW  = 2;
+    // ...
+    localparam BIT_0_CLOCK_HIGH = 62;
+    localparam CLATCH_N_GOING_HIGH = 63;  // AKA BIT_0_CLOCK_LOW
+    localparam WAITING_FOR_CLATCH_N = 64;
+    localparam IDLE = 65;
 
-   reg [6:0] state;
-   assign ready = state == IDLE;
+    reg [6:0] state;
+    reg ready;
 
-   // The clock is low in even states and high in odd states. The only
-   // exception is IDLE - we need to force the clock low there. If we
-   // didn't do this, the clock would be high while IDLE, and low in
-   // the next state. Since the minimum IDLE duration is one CLK
-   // period, the minimum CCLK pulse width might be violated.
-   assign cclk = state[0] && (state != IDLE);
+    // The clock is low in even states and high in odd states. The only
+    // exception is IDLE - we need to force the clock low there. If we
+    // didn't do this, the clock would be high while IDLE, and low in
+    // the next state. Since the minimum IDLE duration is one CLK
+    // period, the minimum CCLK pulse width might be violated.
+    assign cclk = state[0] && (state != IDLE);
 
-   // This shift register holds the data to be transferred. The MSB is
-   // sent first, so we connect it to CDATA and shift to the left.
-   reg [31:0] shiftreg;
-   assign cdata = shiftreg[31];
+    // This shift register holds the data to be transferred. The MSB is
+    // sent first, so we connect it to CDATA and shift to the left.
+    reg [31:0] shiftreg;
+    assign cdata = shiftreg[31];
 
-   always @(posedge clk) begin
-      if(reset) begin
-         state <= IDLE;
-         clatch_n <= 1;
-         divider <= 0;
-      end else begin
-         if(ready) begin
-            if(valid) begin
-               state <= 0;
-               shiftreg <= data_in;
-               clatch_n <= 0;
+    always @(posedge clk, posedge reset) begin
+        if(reset) begin
+            state <= IDLE;
+            clatch_n <= 1;
+            divider <= 0;
+            ready <= 0;
+        end else begin
+            if (state == IDLE) begin
+                ready <= 1;
+                if (ready && valid) begin
+                    state <= 0;
+                    shiftreg <= data_in;
+                    clatch_n <= 0;
+                    ready <= 0;
+                end
+            end else begin
+                if (divider_tick)
+                    divider <= 0;
+                else
+                    divider <= divider + 1;
+
+                if (divider_tick) begin
+                    state <= state + 1;
+                    if (cclk)  // CCLK about to fall?
+                        shiftreg <= {shiftreg[30:0], 1'b0};
+                    if (state == CLATCH_N_GOING_HIGH)
+                        clatch_n <= 1;
+                end
             end
-         end else begin
-            if(divider_tick)
-              divider <= 0;
-            else
-              divider <= divider + 1;
-
-            if(divider_tick) begin
-               state <= state + 1;
-               if(cclk)  // CCLK about to fall?
-                 shiftreg <= {shiftreg[30:0], 1'b0};
-               if(state == CLATCH_N_GOING_HIGH)
-                 clatch_n <= 1;
-            end
-         end
-      end
-   end
-endmodule
+        end
+    end
+    endmodule
--- src/sim/tb_spi_master.v
+++ src/sim/tb_spi_master.v
@@ -1,131 +1,143 @@
 `timescale 1ns / 1ps
 
 module tb_spi_master();
-   wire cclk;
-   wire cdata;
-   wire clatch_n;
-   wire ready;
-   reg clk;
-   reg [31:0] data_in;
-   reg reset;
-   reg valid;
-   adau_spi_master uut
-     (
-      // Outputs
-      .ready (ready),
-      .cdata (cdata),
-      .cclk (cclk),
-      .clatch_n (clatch_n),
-      // Inputs
-      .clk (clk),
-      .reset (reset),
-      .data_in (data_in),
-      .valid (valid)
-      );
+    wire cclk;
+    wire cdata;
+    wire clatch_n;
+    wire ready;
 
-   initial clk <= 0;
-   always #4.167 clk <= ~clk;
+    reg clk;
+    reg [31:0] data_in;
+    reg reset;
+    reg valid;
 
-   specify
-      $period(posedge cclk, 50);
-      $width(posedge clatch_n, 10);
-      $width(negedge clatch_n, 10);
-   endspecify
+    reg [31:0] test_data [0:3];
+    initial begin
+        test_data[0] = 32'habcd0000;
+        test_data[1] = 32'habcd0001;
+        test_data[2] = 32'habcd0002;
+        test_data[3] = 32'habcd0003;
+    end
 
-   always @(cdata) begin
-      if(cclk !== 0)
-        $error("CDATA changed while CCLK is high");
-   end
+    adau_spi_master uut(
+        .ready (ready),
+        .cdata (cdata),
+        .cclk (cclk),
+        .clatch_n (clatch_n),
 
-   task do_reset;
-      begin
-         reset <= 1;
-         repeat(50)
-           @(posedge clk);
-         reset <= 0;
-         @(negedge clk);
-         if(ready !== 1)
-           $error("READY must be high after a reset");
-      end
-   endtask
+        .clk (clk),
+        .reset (reset),
+        .data_in (data_in),
+        .valid (valid)
+    );
 
-   task receive_bits;
-      input [31:0] expected;
-      integer count;
-      reg timeout;
-      reg [31:0] bits;
-      begin
-         count = 0;
-         timeout = 0;
-         timeout <= #50000 1;
-         @(negedge clatch_n or timeout);
-         if(timeout) begin
-            $error("no negative edge on CLATCH_N after waiting for 50us");
-         end else begin
-            while(clatch_n !== 1) begin
-               @(posedge cclk or clatch_n or timeout);
-               if(timeout) begin
-                  $error("transfer took longer than expected (waited for 50us)");
-               end else if(clatch_n === 1) begin
-                  // loop will end
-               end else begin
-                  bits[31 - count] = cdata;
-                  count = count + 1;
-                  if(count > 32)
-                    $error("received more than 32 edges on CCLK");
-               end
+    // Generate clk @5 MHz
+    initial clk <= 0;
+    always #100 clk <= ~clk;
+
+
+    // Receive the data sent by SPI and check against test_data
+    reg [31:0] receive_buf;
+    // To check if clatch goes high
+    reg clatch_was_high = 0;
+    integer receive_bit = 0;
+    integer receive_word = 0;
+
+    initial begin
+        while (1) begin
+            if (clatch_n == 0)
+                clatch_was_high = 1;
+
+            @(posedge cclk) begin
+                if (clatch_n == 0) begin
+                    receive_buf[31 - receive_bit] = cdata;
+                    receive_bit = receive_bit + 1;
+
+                    if (receive_bit == 32) begin
+                        $display("Received data (want: %08x, got: %08x)", test_data[receive_word], receive_buf);
+                        if (clatch_was_high == 0)
+                            $warning({"Clatch did not go high after word. This is OK for SPI, ",
+                                "but make sure you get the 'pull clatch 3 times low' part for ",
+                                "switching to SPI mode right!"});
+                        clatch_was_high = 0;
+
+                        receive_bit = 0;
+                        if (receive_buf != test_data[receive_word]) begin
+                            $error("Received incorrect data (want: %08x, got: %08x)", test_data[receive_word], receive_buf);
+                            $finish;
+                        end
+
+                        receive_word = receive_word + 1;
+                        if (receive_word == 4) begin
+                            $display("Test OK");
+                            $finish;
+                        end
+                        receive_buf = 32'hxxxxxxxx;
+                    end
+                end
             end
-            if(bits !== expected)
-              $error("received incorrect data (want: %08x, got: %08x)", expected, bits);
-         end
-      end
-   endtask
+        end
+    end
 
-   initial begin
-      $timeformat(-9, 5, " ns", 10);
 
-      valid <= 0;
-      do_reset;
+    // Pull reset high for 50 cycles
+    task do_reset;
+        begin
+            // High for 50 cycles
+            reset <= 1;
+            repeat(50) begin
+                @(posedge clk);
+            end
+            reset <= 0;
+        end
+    endtask
 
-      $display("test 1: sending 32'h1234_5678 @ t=%t", $time);
-      data_in <= 32'h1234_5678;
-      valid <= 1;
-      @(posedge clk);
-      fork
-         begin
-            @(posedge clk);
-            if(ready !== 0)
-              $error("READY did not go low after the transfer was started");
-         end
-         begin
-            data_in <= 32'hx;
+
+    // Pull reset high for 50 cycles
+    task do_send;
+        input integer index;
+
+        reg sending;
+        begin
+            sending = 1;
+            valid <= 1;
+
+            data_in <= test_data[index];
+            while (sending == 1) begin
+                @(posedge clk);
+                if (valid == 1 && ready == 1)
+                    sending = 0;
+            end
+
             valid <= 0;
-            receive_bits(32'h1234_5678);
-         end
-      join
-      #1000 ;
-      if(ready !== 1)
-        $error("READY did not go high after the transfer was finished");
+        end
+    endtask
 
-      do_reset;
+    initial begin
+        $timeformat(-9, 5, " ns", 10);
 
-      $display("test 2: sending 32'hf000_00ba, followed by 32'hc000_ffee @ t=%t", $time);
-      data_in <= 32'hf000_0baa;
-      valid <= 1;
-      @(posedge clk);
-      data_in <= 32'hc000_ffee;
-      valid <= 1;
-      fork
-         receive_bits(32'hf000_0baa);
-         begin
-            @(posedge ready);
+        valid <= 0;
+        do_reset;
+
+        // Immediately send after reset
+        do_send(0);
+        // send back-to-back (valid stays high)
+        do_send(1);
+
+        // wait some cycles (valid goes low)
+        repeat(5) begin
+            // May be invalid, as long as valid is low
             @(posedge clk);
-            data_in <= 32'hxxxx_xxxx;
-            valid <= 0;
-         end
-      join
-      receive_bits(32'hc000_ffee);
+            data_in = 31'hxxxxxxxx;
+        end
+        do_send(2);
 
-      $finish;
-   end
+        // Wait until 3rd word has been received
+        while (receive_word != 3)
+            @(receive_word)
+
+        do_reset;
+        do_send(3);
+
+    end
 endmodule
