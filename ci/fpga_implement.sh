070aa8e66ede0b5dd6fc07f9e77a3d4456013142
Johannes Pfau <johannes.pfau@kit.edu>
2019-11-15 14:01:28 +0100

    Update MMI generation scripts to actually work for 4 MiB firmware

--- ci/fpga_implement.sh
+++ ci/fpga_implement.sh
@@ -23,7 +23,8 @@ case "$COMMAND" in
         cp -v psoc_fpga.runs/impl_1/fpga_riscv_top_utilization_placed.rpt out/
         cp -v psoc_fpga.runs/synth_1/runme.log out/synthesis.log
         cp -v psoc_fpga.runs/impl_1/runme.log out/implementation.log
-        cp -v psoc_fpga.mmi out/
+        ./tools/vivado_fix_mmi.py fpga_riscv_top.mmi > fpga_riscv_top.fix.mmi
+        cp -v fpga_riscv_top.fix.mmi out/fpga_riscv_top.mmi
         ;;
     *)
         printf "Unknown command: '%s'\n\n" "$COMMAND" >&2
--- gen_mmi.tcl
+++ gen_mmi.tcl
@@ -3,38 +3,64 @@
 # contents of the RISC-V CPU memory (i.e. the firmware).
 # A few assumptions are made:
 # - The part number is xc7z020clg484-1.
-# - The memory is 8k*32bit = 32KiB big.
-# - It is implemented using 8 RAMB36E1 cells which are all configured as 8k*4bit.
+# - The memory is 4MiB big.
+# - It is implemented using 120 RAMB36E1 cells which are all configured as 32k*1bit.
+# - They are sorted like this:
+#       _0_0_[0-7] are bits 0-7 of word 0   - 32k
+#       _0_1_[0-7] are bits 0-7 of word 32k - 64k
+#       _0_2_[0-7] are bits 0-7 of word 64k - 96k
+#       _0_3_[0-7] are bits 0-7 of word 96k - 128k
+#
+#       _1_0_[0-7] are bits 8-15 of word 0   - 32k
+#       _1_1_[0-7] are bits 8-15 of word 32k - 64k
+#       _1_2_[0-7] are bits 8-15 of word 64k - 96k
+#       _1_3_[0-7] are bits 8-15 of word 96k - 128k
 # - Sorting the cells by their names orders them from low to high output bits.
 #   To get the name of the inferred BRAMs, Vivado postfixes the name of Verilog
-#   memory with "_reg_0_0", "_reg_0_1", "_reg_1_0", ... "_reg_2_1", etc. This
+#   memory with "_reg_0_0_0", "_reg_0_0_1", "_reg_0_1_0", ... "_reg_0_2_1", etc. This
 #   behaviour is not documented, however it seems stable across different versions.
 open_run impl_1
 
-set mmi [open psoc_fpga.mmi w]
+set mmi [open fpga_riscv_top.mmi w]
 
 puts $mmi {
 <?xml version="1.0" encoding="UTF-8"?>
 <MemInfo Version="1" Minor="0">
   <Processor Endianness="Little" InstPath="riscv">
-    <AddressSpace Name="riscv" Begin="0" End="8191">
+    <AddressSpace Name="riscv" Begin="0" End="131071">
       <BusBlock>
 }
 
 set rambs [lsort [get_cells -hierarchical -filter { PRIMITIVE_TYPE == BMEM.bram.RAMB36E1 && NAME =~ "ram/words*" }]]
-set msb 3
-set lsb 0
 
-foreach site [get_property SITE $rambs] {
+foreach ram $rambs {
+    set site [get_property SITE $ram]
+    # Remove RAMB36_ prefix
+    set site [string replace $site 0 [string length "RAMB36_"]-1]
+    set name [get_property NAME $ram]
+    set bram_addr_begin [get_property bram_addr_begin $ram]
+    set bram_addr_end [get_property bram_addr_end $ram]
+    set bram_slice_begin [get_property bram_slice_begin $ram]
+    set bram_slice_end [get_property bram_slice_end $ram]
+
+    set m1 0
+    set mall {}
+    regexp -- {.*words_reg_(\d*)_\d*_\d*} $name mall m1
+
+    set msb [expr $bram_slice_end + (8 * $m1)]
+    set lsb [expr $bram_slice_begin + (8 * $m1)]
+    
+    # Convert addresses to bytes
+    set bram_addr_begin [expr $bram_addr_begin * 4]
+    set bram_addr_end [expr (($bram_addr_end + 1) * 4 ) - 1]
+
     puts $mmi "
         <BitLane MemType=\"RAMB32\" Placement=\"$site\">
           <DataWidth MSB=\"$msb\" LSB=\"$lsb\" />
-          <AddressRange Begin=\"0\" End=\"8191\" />
+          <AddressRange Begin=\"$bram_addr_begin\" End=\"$bram_addr_end\" />
           <Parity ON=\"false\" NumBits=\"0\" />
         </BitLane>
     "
-    set msb [expr $msb + 4]
-    set lsb [expr $lsb + 4]
 }
 
 puts $mmi {
