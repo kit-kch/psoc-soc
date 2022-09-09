c4700a6c2bde50625ff28a5e4de2311eb8d7d6b9
nv3401 <marc.neu@kit.edu>
2022-09-09 10:44:58 +0200

    Added Update Project tcl script

--- prj/update_prj.tcl
+++ prj/update_prj.tcl
@@ -1,31 +1,43 @@
-#Load Project
-source ../prj/gen_prj.tcl
-
 #Add VHDL Design Sources to Project
-set file_list [glob -directory ../src/hdl/ *.vhd]
-read_vhdl $file_list
+set file_list [glob -nocomplain -directory ../src/hdl/ *.vhd]
+if {[llength $file_list] != 0} {
+    read_vhdl $file_list
+} else {
+    puts "No VHDL Design Sources found."
+}
 
 #Add Verilog Design Sources to Project
-set file_list [glob -directory ../src/hdl/ *.v]
-read_verliog $file_list
+set file_list [glob -nocomplain -directory ../src/hdl/ *.v]
+if {[llength $file_list] != 0} {
+    read_verilog $file_list
+} else {
+    puts "No Verilog Design Sources found."
+}
 
 #Add VHDL Sim Sources to Project
-set file_list [glob -directory ../src/sim/ *.vhd]
-read_vhdl $file_list
-
+set file_list [glob -nocomplain -nocomplain -directory ../src/sim/ *.vhd]
+if {[llength $file_list] != 0} {
+    read_vhdl $file_list
+} else {
+    puts "No VHDL Sim Sources found."
+}
 #Add Verilog Sim Sources to Project
-set file_list [glob -directory ../src/sim/ *.v]
-read_verliog $file_list
+set file_list [glob -nocomplain -directory ../src/sim/ *.v]
+if {[llength $file_list] != 0} {
+    read_verilog $file_list
+} else {
+    puts "No Verilog Sim Sources found."
+}
 
 #Update Riscv Constraints File
-set_property constrset constr_riscv [get_runs synth_1]
-set_property constrset constr_riscv [get_runs impl_1]
+set_property constrset constrs_riscv [get_runs synth_1]
+set_property constrset constrs_riscv [get_runs impl_1]
 set file_list [glob -directory ../src/constraints/ constr_riscv.xdc]
 read_xdc $file_list
 
 #Update Standalone Constraints File
-set_property constrset constr_standalone [get_runs synth_1]
-set_property constrset constr_standalone [get_runs impl_1]
+set_property constrset constrs_standalone [get_runs synth_1]
+set_property constrset constrs_standalone [get_runs impl_1]
 set file_list [glob -directory ../src/constraints/ constr_standalone.xdc]
 read_xdc $file_list
 
@@ -36,7 +48,4 @@ update_compile_order -fileset sources_1
 update_compile_order -fileset sim_1
 
 #Write project tcl file. Use -force to overwrite
-write_project_tcl -no_copy_sources -use_bd_files -force {./prj/gen_prj.tcl}
-
-#Close project and finish
-close_project
+write_project_tcl -no_copy_sources -use_bd_files -force {../prj/gen_prj.tcl}
