d19b77e44196783cda688f850dc255d0ce0b665c
Johannes Pfau <johannes.pfau@kit.edu>
2022-09-13 09:53:50 +0200

    Support sourcing of the vivado_env script

--- prj/vivado_env.sh
+++ prj/vivado_env.sh
@@ -5,4 +5,7 @@ if ! [ -x "$(command -v vivado)" ]; then
     source /tools/xilinx/set_xilinx2022.1.sh &>/dev/null
 fi
 
-exec "$@"
+# Allow sourcing this script without params. Otherwise exec command
+if [ $# -ne 0 ]; then
+    exec "$@"
+fi
