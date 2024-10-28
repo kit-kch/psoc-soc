056dd4ebe63da9d9ee04b9d8051f0ed28281bbc0
hn6196 <alexey.serdyuk@kit.edu>
2024-10-28 19:30:18 +0000

    Fix bitstream generation

--- script/vivado/standalone_bit.tcl
+++ script/vivado/standalone_bit.tcl
@@ -4,5 +4,5 @@ set_property constrset constrs_standalone [get_runs synth_1]
 set_property constrset constrs_standalone [get_runs impl_1]
 reset_run -quiet synth_1
 reset_run -quiet impl_1
-launch_runs -jobs 8 -to_step write_bitstream impl_1
+launch_runs -to_step write_bitstream impl_1
 wait_on_run impl_1
