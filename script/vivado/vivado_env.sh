2bdf9709cce203bc876e6cdc0caa29cdbfc4e7cb
hn6196 <alexey.serdyuk@kit.edu>
2024-10-28 17:40:20 +0000

    Updated Vivado version

--- script/vivado/vivado_env.sh
+++ script/vivado/vivado_env.sh
@@ -2,7 +2,7 @@
 
 # Only source vivado if this was not done before
 if ! [ -x "$(command -v vivado)" ]; then
-    source /tools/xilinx/set_xilinx2022.1.sh &>/dev/null
+    module load xilinx/2024.1
 fi
 
 # Allow sourcing this script without params. Otherwise exec command
