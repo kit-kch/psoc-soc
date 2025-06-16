3630fa4dd39f448fd4146934bd618ec678f0bfea
Johannes Pfau <johannes.pfau@kit.edu>
2025-06-16 11:03:15 +0200

    I2S regfile: Only check the 16 low address bits
    Do not care about high address  bits so we can configure the peripheral to be anywhere in the address space

--- src/hdl/i2s_wb_regfile.v
+++ src/hdl/i2s_wb_regfile.v
@@ -4,21 +4,21 @@
 
 /*
  * MEMORY MAP:
- * - 0x90000000: Control register CTRL0
+ * - 0x0000: Control register CTRL0
  *   - Bit 0: Reset (1 = active)
  *   - Bit 1: DAC mode: 0 = I2S output, 1 = Use builtin DAC
  *   - Bit 2: DAC enable
  *   - Bit 3: I2S enable
- * - 0x90000004: Status register STAT0 (readonly)
+ * - 0x0004: Status register STAT0 (readonly)
  *   - Bit 0: FIFO is low (1 when Fifo count is less than FIFO_LOW)
  *   - Bit 1: FIFO empty (1 if FIFO is empty)
  *   - Bit 2: FIFO full (1 if FIFO is full)
- * - 0x90000008: FIFO low threshold FIFO_LOW
- * - 0x9000000c: FIFO level FIFO_LEVEL (readonly)
- * - 0x90000010: AUDIO_LEFT (write-only)
+ * - 0x0008: FIFO low threshold FIFO_LOW
+ * - 0x000c: FIFO level FIFO_LEVEL (readonly)
+ * - 0x0010: AUDIO_LEFT (write-only)
  *   - Bit 0-23: left audio sample
  *   - Bit 31: Set to 1 to write out all 48 bits of audio data
- * - 0x90000014: AUDIO_RIGHT (write-only)
+ * - 0x0014: AUDIO_RIGHT (write-only)
  *   - Bit 0-23: right audio sample
  *   - Bit 31: Set to 1 to write out all 48 bits of audio data
  */
@@ -69,18 +69,18 @@ module i2s_wb_regfile #(
     assign fifo_threshold[FIFO_LEN_BITS:0] = fifo_threshold_reg[FIFO_LEN_BITS:0];
 
     // read logic
-    always @(posedge clk) begin    
-        case (wb_adr_i)
-            32'h9000_0000: begin
+    always @(posedge clk) begin
+        case (wb_adr_i[15:0])
+            16'h0000: begin
                 wb_dat_o <= reg_ctrl0;
             end
-            32'h9000_0004: begin
+            16'h0004: begin
                 wb_dat_o <= {29'b0, fifo_full, fifo_empty, fifo_low};
             end
-            32'h9000_0008: begin
+            16'h0008: begin
                 wb_dat_o <= fifo_threshold_reg;
             end
-            32'h9000_000c: begin
+            16'h000c: begin
                 wb_dat_o <= {{32-FIFO_LEN_BITS{1'b0}}, fifo_level};
             end
             default: begin
@@ -100,12 +100,12 @@ module i2s_wb_regfile #(
             audio_valid <= 1'b0;
 
             if (wb_stb_i && wb_we_i && !o_wb_stall) begin
-                case (wb_adr_i)
-                    32'h9000_0000: begin
+                case (wb_adr_i[15:0])
+                    32'h0000: begin
                         if (wb_sel_i[0])
                             reg_ctrl0[7:0] <= wb_dat_i[7:0];
                     end
-                    32'h9000_0008: begin
+                    16'h0008: begin
                         if (wb_sel_i[3])
                             fifo_threshold_reg[31:24] <= wb_dat_i[31:24];
                         if (wb_sel_i[2])
@@ -115,7 +115,7 @@ module i2s_wb_regfile #(
                         if (wb_sel_i[0])
                             fifo_threshold_reg[7:0] <= wb_dat_i[7:0];
                     end
-                    32'h9000_0010: begin
+                    16'h0010: begin
                         if (wb_sel_i[3])
                             audio_valid <= wb_dat_i[31];
                         if (wb_sel_i[2])
@@ -125,7 +125,7 @@ module i2s_wb_regfile #(
                         if (wb_sel_i[0])
                             audio_data[7:0] <= wb_dat_i[7:0];
                     end
-                    32'h9000_0014: begin
+                    16'h0014: begin
                         if (wb_sel_i[3])
                             audio_valid <= wb_dat_i[31];
                         if (wb_sel_i[2])
