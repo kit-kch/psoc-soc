a9de5e6dcbdaeb132d8162d43c082a65b572f927
Johannes Pfau <johannes.pfau@kit.edu>
2025-06-18 09:30:41 +0200

    IHP: Use SRAM library

--- script/sg13g2/config.mk
+++ script/sg13g2/config.mk
@@ -2,6 +2,10 @@ export DESIGN_NAME = soc_top
 export DESIGN_PATH := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))../../
 export PLATFORM    = ihp-sg13g2
 
+export ADDITIONAL_LEFS += $(PLATFORM_DIR)/lef/RM_IHPSG13_1P_4096x16_c3_bm_bist.lef
+export ADDITIONAL_LIBS += $(PLATFORM_DIR)/lib/RM_IHPSG13_1P_4096x16_c3_bm_bist_typ_1p20V_25C.lib
+export ADDITIONAL_GDS += $(PLATFORM_DIR)/gds/RM_IHPSG13_1P_4096x16_c3_bm_bist.gds
+
 RTL_DIR = $(DESIGN_PATH)src/hdl
 SCRIPT_DIR = $(DESIGN_PATH)script/sg13g2
 
@@ -24,4 +28,4 @@ export CORE_UTILIZATION = 60
 export TNS_END_PERCENT = 100
 
 # FIXME
-export SYNTH_MEMORY_MAX_BITS = 20000000
+export SYNTH_MEMORY_MAX_BITS = 12288
