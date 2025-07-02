f914962402b7ee2806363421db7fa1781b1a1fa3
Johannes Pfau <johannes.pfau@kit.edu>
2025-07-02 14:29:13 +0200

    Fix audio FIFO address length

--- src/hdl/sg13g2/sfifo_mem.v
+++ src/hdl/sg13g2/sfifo_mem.v
@@ -15,11 +15,11 @@ module sfifo_mem(i_clk, i_wr, i_wr_addr, i_data, i_rd, i_rd_addr, o_data);
 	// Write interface
 	input	wire		i_wr;
 	input	wire [(BW-1):0]	i_data;
-	input   wire [LGFLEN:0] i_wr_addr;
+	input   wire [LGFLEN-1:0] i_wr_addr;
 	// Read interface
 	input	wire		i_rd;
 	output	reg [(BW-1):0]	o_data;
-	input   wire [LGFLEN:0] i_rd_addr;
+	input   wire [LGFLEN-1:0] i_rd_addr;
 
 	initial begin
 		//assert (BW == 48) else $fatal("Parameter BW must be 48");
