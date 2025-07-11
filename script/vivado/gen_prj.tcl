0cb029601be62c5ddedbf3dee20e5fb07f5e2383
Johannes Pfau <johannes.pfau@kit.edu>
2025-07-11 13:43:31 +0200

    Remove unused led.v from Vivado project

--- script/vivado/gen_prj.tcl
+++ script/vivado/gen_prj.tcl
@@ -96,7 +96,6 @@
 #    "/home/nq5949/Dokumente/psoc/psoc_soc2/src/hdl/fpga/fpga_soc_top.v"
 #    "/home/nq5949/Dokumente/psoc/psoc_soc2/src/hdl/fpga/fpga_standalone_top.v"
 #    "/home/nq5949/Dokumente/psoc/psoc_soc2/src/init/sin_lut_90x24.mem"
-#    "/home/nq5949/Dokumente/psoc/psoc_soc2/src/hdl/led.v"
 #    "/home/nq5949/Dokumente/psoc/psoc_soc2/src/hdl/audio/sine_generator.v"
 #    "/home/nq5949/Dokumente/psoc/psoc_soc2/script/vivado/constr_standalone.xdc"
 #    "/home/nq5949/Dokumente/psoc/psoc_soc2/script/vivado/constr_soc.xdc"
@@ -184,7 +183,6 @@ proc checkRequiredFiles { origin_dir} {
  "[file normalize "$origin_dir/../../src/hdl/fpga/fpga_soc_top.v"]"\
  "[file normalize "$origin_dir/../../src/hdl/fpga/fpga_standalone_top.v"]"\
  "[file normalize "$origin_dir/../../src/init/sin_lut_90x24.mem"]"\
- "[file normalize "$origin_dir/../../src/hdl/led.v"]"\
  "[file normalize "$origin_dir/../../src/hdl/audio/sine_generator.v"]"\
  "[file normalize "$origin_dir/../../script/vivado/constr_standalone.xdc"]"\
  "[file normalize "$origin_dir/../../script/vivado/constr_soc.xdc"]"\
@@ -386,7 +384,6 @@ set files [list \
  [file normalize "${origin_dir}/../../src/hdl/fpga/fpga_soc_top.v"] \
  [file normalize "${origin_dir}/../../src/hdl/fpga/fpga_standalone_top.v"] \
  [file normalize "${origin_dir}/../../src/init/sin_lut_90x24.mem"] \
- [file normalize "${origin_dir}/../../src/hdl/led.v"] \
  [file normalize "${origin_dir}/../../src/hdl/audio/sine_generator.v"] \
 ]
 add_files -norecurse -fileset $obj $files
