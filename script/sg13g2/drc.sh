7e01e7dddafb53db93867d5ab4555995741c6d97
Johannes Pfau <johannes.pfau@kit.edu>
2025-07-30 10:54:25 +0200

    SG13G2: DRC: Properly save logs and fail on DRC errors

--- script/sg13g2/drc.sh
+++ script/sg13g2/drc.sh
@@ -5,4 +5,8 @@ PDKPATH=/foss/pdks/ihp-sg13g2
 DRC_DIR=$PDKPATH/libs.tech/klayout/tech/drc
 RESULT_DIR=$FLOW_HOME/results/ihp-sg13g2/soc_top/base
 
-klayout -n sg13g2 -zz -r $DRC_DIR/sg13g2_minimal.lydrc -rd "in_gds=$RESULT_DIR/7_filled.gds" -rd "log_file=$RESULT_DIR/8_drc_sg13g2_minimal.log" -rd "report_file=$RESULT_DIR/8_drc_sg13g2_minimal.lyrdb"
+klayout -n sg13g2 -zz -r $DRC_DIR/sg13g2_minimal.lydrc -rd "in_gds=$RESULT_DIR/7_filled.gds" -rd "log_file=$RESULT_DIR/8_drc_sg13g2_minimal.2.log" -rd "report_file=$RESULT_DIR/8_drc_sg13g2_minimal.lyrdb"  | tee $RESULT_DIR/8_drc_sg13g2_minimal.log
+
+# Check that there are 0 violations
+cat $RESULT_DIR/8_drc_sg13g2_maximal.log | grep -q "Number of DRC errors: 0"
+exit $?
--- script/sg13g2/drc_full.sh
+++ script/sg13g2/drc_full.sh
@@ -5,4 +5,8 @@ PDKPATH=/foss/pdks/ihp-sg13g2
 DRC_DIR=$PDKPATH/libs.tech/klayout/tech/drc
 RESULT_DIR=$FLOW_HOME/results/ihp-sg13g2/soc_top/base
 
-klayout -n sg13g2 -zz -r $DRC_DIR/sg13g2_maximal.lydrc -rd "in_gds=$RESULT_DIR/7_filled.gds" -rd "log_file=$RESULT_DIR/8_drc_sg13g2_maximal.log" -rd "report_file=$RESULT_DIR/8_drc_sg13g2_maximal.lyrdb"
+klayout -n sg13g2 -zz -r $DRC_DIR/sg13g2_maximal.lydrc -rd "in_gds=$RESULT_DIR/7_filled.gds" -rd "log_file=$RESULT_DIR/8_drc_sg13g2_maximal.2.log" -rd "recommendedRules=false" -rd "report_file=$RESULT_DIR/8_drc_sg13g2_maximal.lyrdb" | tee $RESULT_DIR/8_drc_sg13g2_maximal.log
+
+# Check that there are 0 violations
+cat $RESULT_DIR/8_drc_sg13g2_maximal.log | grep -q "Number of DRC errors: 0"
+exit $?
